2cdbff80b9105b296b8c17ad81b3c2a595e55d61
Your Name <you@example.com>
2020-10-08 11:11:33 +0100

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ void on_cleanup()
 
 int main(int argc, char* args[])
 {
-	util::sdl_initialize("Introduction Game (C++/SDL)", 800, 600);
+	util::sdl_initialize("rastacllaut)", 800, 600);
 
 	on_load();
 	SDL_SetWindowIcon(util::sdl_window, icon);
